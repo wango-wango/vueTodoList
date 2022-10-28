f7ed59d62279d6c8c682c2be4f2d95bfc6ac7684
Jerry Wwang <doghost111@gmail.com>
2022-10-29 00:15:09 +0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./vueTodoList",
+    base: "./vueTodoList/",
     plugins: [vue()],
 });
